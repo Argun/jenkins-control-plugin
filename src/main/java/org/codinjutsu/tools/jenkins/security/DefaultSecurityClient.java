afbc16bc9ec1a8f8e349fa0b6c159b46f4bf6b60
sunjz <sunjz@nextop.cn>
2022-05-19 17:03:29 +0800

    disable verify host name

--- src/main/java/org/codinjutsu/tools/jenkins/security/DefaultSecurityClient.java
+++ src/main/java/org/codinjutsu/tools/jenkins/security/DefaultSecurityClient.java
@@ -44,8 +44,18 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Collection;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 class DefaultSecurityClient implements SecurityClient {
 
@@ -57,6 +67,7 @@ class DefaultSecurityClient implements SecurityClient {
     protected JenkinsVersion jenkinsVersion = JenkinsVersion.VERSION_1;
 
     DefaultSecurityClient(String crumbData, int connectionTimout) {
+        disableSSLVerification();
         this.httpClient = new HttpClient(new MultiThreadedHttpConnectionManager());
         this.crumbData = crumbData;
         this.connectionTimout = connectionTimout;
@@ -216,5 +227,38 @@ class DefaultSecurityClient implements SecurityClient {
             this.data = body;
         }
 
+    }
+    public static void disableSSLVerification() {
+
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+
+        } };
+
+        SSLContext sc = null;
+        try {
+            sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
     }
 }
